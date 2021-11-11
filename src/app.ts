936deca5fbc23087a517a49f03a60e76e4bbd00e
Lakshmi Narayana <studygouthamm@gmail.com>
2021-11-11 15:52:20 +0530

    Interfaces with Index Signatures

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ interface Sizes {
   sizes: string[];
 }
 
-interface Pizza  extends Sizes {
+interface Pizza extends Sizes {
   name: string;
+  toppings?: number;
   getAvailableSizes(): string[];
+  [key: number]: string
 }
 
 let pizza: Pizza;
@@ -20,5 +22,7 @@ function createPizza(name: string, sizes: string[]) {
 }
 
 pizza = createPizza('Pepperoni', ['small', 'large', 'medium']);
+pizza[1] = 'xyz'
 
+pizza.toppings = 1
 console.log(pizza);
