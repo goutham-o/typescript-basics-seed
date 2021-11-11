18bc81146519006be30047aa770393164b365c9e
Lakshmi Narayana <studygouthamm@gmail.com>
2021-11-11 16:27:03 +0530

    Readonly Members, Setters and Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
-class Pizza {
-  name: string;
-  toppings: string[] = [];
-  constructor(name: string) {
-    this.name = name;
+
+class Sizes {
+  constructor(public sizes: string[]) { }
+  
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
   }
+  
+  get availableSizes() {
+    return this.sizes;
+  }
+
+}
+
+const sizes = new Sizes(['small', 'medium'])
+
+//invoke getter
+console.log(sizes.availableSizes)
+//invoke setter
+sizes.availableSizes = ['large', 'extra large'];
+console.log(sizes.availableSizes)
+
+
+class Pizza {
+  toppings: string[] = [];
+
+  // constructor(public name: string) {}
+  constructor(readonly name: string) {}
 
   addTopping(topping: string) {
     this.toppings.push(topping);
