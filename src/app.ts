90aaac2bb9abb5f084c2be44b90d2ece4e5e16ca
Lakshmi Narayana <studygouthamm@gmail.com>
2021-11-11 12:24:58 +0530

    functiontype

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-let pizzaSize: string = 'small';
+let sumOrder: (price: number, quantity: number) => number;
 
-function selectSize(size: 'small' | 'medium' | 'large'): void {
-  pizzaSize = size;
-}
+sumOrder = (x, y) => x * y;
 
-selectSize('large');
+const sum = sumOrder(234, 2);
 
-console.log(`pizza size ${pizzaSize}`)
+console.log(`Total sum: ${sum}`);
